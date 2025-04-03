dd300002177f2aca6e897ff0eefdde4d91b3e27c
Kate <kit-ty-kate@outlook.com>
2025-04-03 23:55:18 +0100

    Upgrade to mirage-crypto 2.0.0

--- server/backend/backend.ml
+++ server/backend/backend.ml
@@ -138,7 +138,7 @@ let start ~debug conf workdir =
   let run_trigger = Lwt_mvar.create_empty () in
   let callback = Admin.callback ~on_finished ~conf ~run_trigger workdir in
   Lwt.ignore_result (cache_clear_and_init workdir);
-  Mirage_crypto_rng_lwt.initialize (module Mirage_crypto_rng.Fortuna);
+  Mirage_crypto_rng_unix.use_default ();
   let%lwt () = Admin.create_admin_key workdir in
   let task () =
     Lwt.join [
